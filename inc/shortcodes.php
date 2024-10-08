84fd4a17c9aeb19b075aa62f1799ba6352a66ea7
Arekk17 <daszkowskiarek145@gmail.com>
2024-10-08 14:18:25 +0200

    fixed theme

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -1,25 +1,12 @@
-<<<<<<< Updated upstream
 <?php
 
 function about_me_section_shortcode()
 {
     ob_start();
-    get_template_part('template-parts/About/about-me-text');
+    get_template_part('template-parts/about/about-me');
     return ob_get_clean();
 }
 add_shortcode('about_me_section', 'about_me_section_shortcode');
-<<<<<<< HEAD
-=======
-<?php
-
-// function about_me_section_shortcode()
-// {
-//     ob_start();
-//     get_template_part('template-parts/About/about-me-text');
-//     return ob_get_clean();
-// }
-// add_shortcode('about_me_section', 'about_me_section_shortcode');
-
 
 function notarial_actions_shortcode()
 {
@@ -28,15 +15,3 @@ function notarial_actions_shortcode()
     return ob_get_clean();
 }
 add_shortcode('notarial_actions', 'notarial_actions_shortcode');
->>>>>>> Stashed changes
-=======
-
-
-function notarial_actions_shortcode()
-{
-    ob_start();
-    get_template_part('template-parts/notarial-actions/notarial-actions');
-    return ob_get_clean();
-}
-add_shortcode('notarial_actions', 'notarial_actions_shortcode');
->>>>>>> 24d26882094ae9e2f5f80f46cb35df32cd974ef9
--- template-parts/buttons/button.php
+++ template-parts/buttons/button.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 if (!function_exists('render_custom_button')) {
     function render_custom_button($text, $url)
@@ -10,17 +9,4 @@ if (!function_exists('render_custom_button')) {
 <?php
     }
 }
-=======
-<?php
-if (!function_exists('render_custom_button')) {
-    function render_custom_button($text, $url)
-    {
-?>
-        <button class="custom-button" onclick="window.location.href='<?php echo esc_url($url); ?>'">
-            <?php echo esc_html($text); ?>
-        </button>
-<?php
-    }
-}
->>>>>>> 24d26882094ae9e2f5f80f46cb35df32cd974ef9
 ?>
